241a9baab35e7aa21b1fa51851f101a820b6d5c4
zisdead1 <davegarrehy@gmail.com>
2020-09-14 18:19:52 +0100

    Dynamically get pokemon name from GET request

--- Node_Pokemon_API.js
+++ Node_Pokemon_API.js
@@ -1,10 +1,14 @@
 var express = require("express");
 var app = express();
 
-//testing that localhost server returns some JSON
-//i.e I have my first node webserver with an endpoint 
-app.get("/url", (req, res, next) => {
-    res.json(["Dave","Garrehy","Food"]);
+//For parsing out the pokemon entered in URL
+//const url = require('url');
+
+app.get("/pokemon/:pokemonid", (req, res, next) => {
+    //Dynamically get the pokemon character which will be 
+    //last param in the url
+    res.send("pokemon is " + req.params.pokemonid);
+    //res.json(["Dave","Garrehy","Food"]);
 });
 
 app.listen(3000, () => {
