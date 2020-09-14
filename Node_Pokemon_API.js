a614704278445cb0867040bf82b5cad92e1f7360
zisdead1 <davegarrehy@gmail.com>
2020-09-14 17:12:26 +0100

    Testing HTTP GET endpoint

--- Node_Pokemon_API.js
+++ Node_Pokemon_API.js
@@ -1,5 +1,12 @@
 var express = require("express");
 var app = express();
+
+//testing that localhost server returns some JSON
+//i.e I have my first node webserver with an endpoint 
+app.get("/url", (req, res, next) => {
+    res.json(["Dave","Garrehy","Food"]);
+});
+
 app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
