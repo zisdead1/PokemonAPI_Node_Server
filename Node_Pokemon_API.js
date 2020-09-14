ff58fe71a1871f70de910324eabc1d13213a9d39
zisdead1 <davegarrehy@gmail.com>
2020-09-14 23:50:41 +0100

    Shakespeare call close to working

--- Node_Pokemon_API.js
+++ Node_Pokemon_API.js
@@ -1,4 +1,5 @@
-const { Console } = require("console");
+//const { Console } = require("console");
+var axios = require('axios');
 var express = require("express");
 var app = express();
 
@@ -11,10 +12,6 @@ app.get("/pokemon/:pokemonid", (req, res, next) => {
     //Dynamically get the pokemon character which will be 
     //last param in the url
     var pokemon = req.params.pokemonid;
-    if(pokemon === "")
-    {
-        throw new Error("A pokemon string was not included in the URL");
-    }
 
     //now need to make a call to the 
     //PokeAPI to get a description for the
@@ -23,11 +20,28 @@ app.get("/pokemon/:pokemonid", (req, res, next) => {
     //has an embedded description of a given pokemon
     P.getPokemonSpeciesByName(pokemon, function(response, error) { // with callback
         if(!error) {
-          res.json(response.flavor_text_entries[6].flavor_text);
+            //this is where we find a description in the returned object
+            var pokemonDesc = response.flavor_text_entries[6].flavor_text;
+            //Now make call to Shakespeare translator API 
+            var apiBaseString = "https://api.funtranslations.com/translate/shakespeare.json?text=";
+            //Take care of escaping the relavant characters in the string
+            var escapedStr = encodeURI(apiBaseString+pokemonDesc);
+            //res.send(escapedStr);
+            //console.log(escapedStr);
+            makePostRequest(escapedStr);
         } else {
-          res.status(404).json('Sorry we dont have that Pokemon');
+            res.status(404).json('Sorry we dont have that Pokemon');
         }
     });
+
+    async function makePostRequest(url) {
+        //Generic POST HTTP call
+        var escapedStr = encodeURI(url);
+        //console.log(escapedStr);
+        let postRes = await axios.post("https://api.funtranslations.com/translate/shakespeare.json?text=CHARIZARD%20flies%20around%20the%20sky");
+        res.send(postRes.data);
+        console.log(res.data);
+    }
 });
 
 app.listen(3000, () => {
