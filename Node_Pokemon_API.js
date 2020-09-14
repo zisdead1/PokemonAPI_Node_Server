39f5c1fb914ee6cd204c11b1e4614be77ffe31e6
zisdead1 <davegarrehy@gmail.com>
2020-09-14 20:03:50 +0100

    A good callback from PokeAPI with hard coding

--- Node_Pokemon_API.js
+++ Node_Pokemon_API.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 //Needed to call the PokeAPI using Node
-//friendly interface
+//friendly wrapper
 var Pokedex = require('pokedex-promise-v2');
 var P = new Pokedex();
 
@@ -14,10 +14,18 @@ app.get("/pokemon/:pokemonid", (req, res, next) => {
     {
         throw new Error("A pokemon string was not included in the URL");
     }
-    res.send("pokemon is " + pokemon);
+
     //now need to make a call to the 
     //PokeAPI to get a description for the
     //pokemon character sent in the URL
+    P.getPokemonByName(34, function(response, error) { // with callback
+        if(!error) {
+          res.json(response);
+        } else {
+          res.send(error);
+        }
+    });
+    
     
     //res.json(["Dave","Garrehy","Food"]);
 });
